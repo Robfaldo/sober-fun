0a745a2fec9fd0d0aa71857f5152aa269a98686f
Robert Faldo <robertfaldo@gmail.com>
2018-09-30 17:35:27 +0100

    add events controller and add event form in view

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'event_listings/index'
 
+  resources :events
+  
   root 'event_listings#index'
 end
