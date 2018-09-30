5b046d77e8e7d6568e80ec5f64f408caa51eb38b
Robert Faldo <robertfaldo@gmail.com>
2018-09-30 18:15:46 +0100

    Remove the events_listing controller
    Just use the events index instead

--- app/helpers/event_listings_helper.rb
+++
@@ -1,2 +0,0 @@
-module EventListingsHelper
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'event_listings/index'
+  get 'events/index'
 
   resources :events
-  
-  root 'event_listings#index'
+  root 'events#index'
 end
