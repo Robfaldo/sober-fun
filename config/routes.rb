d2f05a7e4d8121e6b0894d96919b9c5409fea6f7
Robert Faldo <robertfaldo@gmail.com>
2018-09-30 17:21:51 +0100

    Add event_listings controller and page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'event_listings/index'
+
+  root 'event_listings#index'
 end
