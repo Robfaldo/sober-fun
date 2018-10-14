afe27453cc2c519b1a9651beefe4b2b3b67599ef
Robert Faldo <robertfaldo@gmail.com>
2018-10-14 10:13:13 +0100

    Events can be tagged

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'events/index'
 
+  get 'events/index'
   resources :events
-  
   root 'events#index'
+  get 'tags/:tag', to: 'events#index', as: :tag
 end
